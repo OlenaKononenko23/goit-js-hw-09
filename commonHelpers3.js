f1d3eb7185a81d2620c0f25386470f51ecebb966
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-03-16 15:39:02 +0000

    Deploying to gh-pages from @ OlenaKononenko23/goit-js-hw-09@2eeadc855bc1198713efd13db6728532d841ffa7 🚀

---
+++ commonHelpers3.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import"./assets/1-gallery-7ed95581.js";import"./assets/2-form-c9317120.js";import"./assets/vendor-870f0eb5.js";const o=document.querySelectorAll(".linkList");o.forEach(t=>{t.addEventListener("click",e=>{e.preventDefault();const r=t.querySelector("a").getAttribute("href");window.location.href=r})});
+//# sourceMappingURL=commonHelpers3.js.map
